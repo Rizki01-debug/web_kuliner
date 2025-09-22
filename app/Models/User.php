9c4db7b15c83383c705e7671ca7aa30f172f664e
Rizki01-debug <subagjamuhammadrizky@gmail.com>
2025-09-22 22:05:59 +0700

    Menambahkan kodingan seeder dan membuat middleware IsAdmin "RizkiCreation"

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use PhpParser\Node\Expr\Cast;
 
 class User extends Authenticatable
 {
@@ -21,6 +22,12 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'is_admin',
+    ];
+
+    protected $casts = [
+        'email_veriefird_at' => 'datetime',
+        'is_admin' => 'boolean',
     ];
 
     /**
